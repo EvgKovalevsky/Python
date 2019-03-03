e2b74aaf20cc6b29d6b0b7f0b5ee3b44b25ee33e
Evgeny <43949028+EvgKovalevsky@users.noreply.github.com>
2019-03-03 13:21:12 +0300

    Add files via upload

---
+++ HW1/Equation.py
@@ -0,0 +1,28 @@
+import math
+from math import sqrt
+A = float(input("A = "))
+B = float(input("B = "))
+C = float(input("C = "))
+
+def Equation(a, b, c):
+	if (a == 0 and b == 0):
+		return "Корней не существует."
+	elif (a == 0):
+		return (-c / b)
+	else:
+		D = b**2 - 4*a*c
+		if(D > 0):
+			return (-b + sqrt(D)) / (2 * a), (-b - sqrt(D)) / (2 * a)
+		elif (D == 0):
+			return (-b / (2 * a))
+		else:
+			return "D < 0 - Корней нет."
+
+print(Equation(A, B, C))
+
+#Открытые тесты:
+
+#Equation(2, 5, 2)
+#Equation(2, 4, 2)
+#Equation(2, 1, 2)
+#Equation(0, 4, 10)
